90e9bcdfa2ba855333d009d1b3f323bb0c061ee8
Elwin Stephan <elwin.stephan@gmail.com>
2018-03-09 23:57:50 +0100

    sadf

--- app/Http/Controllers/FactController.php
+++ app/Http/Controllers/FactController.php
@@ -45,7 +45,7 @@ class FactController extends Controller
         ]);
 
         $user->messages()->create([
-            'message' => $request->message
+            'message' => $request->message['text']
         ]);
 
         $update = Telegram::commandsHandler(true);
