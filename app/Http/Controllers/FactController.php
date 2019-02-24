6237eae784796da906d424755f0702ce5f64abb5
Elwin Stephan <elwin.stephan@gmail.com>
2019-02-24 12:11:21 +0100

    Added another log

--- app/Http/Controllers/FactController.php
+++ app/Http/Controllers/FactController.php
@@ -38,6 +38,8 @@ class FactController extends Controller
 
     public function webhook(Request $request)
     {
+        log('received message');
+
         $user = User::firstOrCreate([
             'id' => $request->message['from']['id'],
             'name' => $request->message['from']['first_name'],
